e228035e4436a769fd32b4983739154af5684c8b
Alberto Rosas <alberto.rsesc@protonmail.com>
2022-12-10 15:56:21 -0800

    A Sale can be approved

--- test/Escrow.js
+++ test/Escrow.js
@@ -119,4 +119,22 @@ describe('Escrow', () => {
 			expect(response).to.be.equal(true);
 		});
 	});
+
+	// Approval
+	describe('Approval', () => {
+		it('Updates approval status', async () => {
+			let transaction = await escrow.connect(buyer).approveSale(1);
+			await transaction.wait();
+
+			transaction = await escrow.connect(seller).approveSale(1);
+			await transaction.wait();
+
+			transaction = await escrow.connect(lender).approveSale(1);
+			await transaction.wait();
+
+			expect(await escrow.approval(1, buyer.address)).to.be.equal(true);
+			expect(await escrow.approval(1, seller.address)).to.be.equal(true);
+			expect(await escrow.approval(1, lender.address)).to.be.equal(true);
+		});
+	});
 });
